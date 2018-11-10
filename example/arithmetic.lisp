192bfa97694e602f711bad049f74c6194bd9f2ee
carrotflakes <carrotflakes@gmail.com>
2018-11-10 17:56:40 +0900

    add example/arithmetic.lisp

---
+++ example/arithmetic.lisp
@@ -0,0 +1,26 @@
+(ql:quickload :snaky)
+
+(use-package :snaky)
+
+(defrule arithmetic (and ws additive ws))
+
+(defrule additive
+  (or (mod (and additive ws "+" ws multiplicative) #'+)
+      (mod (and additive ws "-" ws multiplicative) #'-)
+      multiplicative))
+
+(defrule multiplicative
+  (or (mod (and multiplicative ws "*" ws primary) #'*)
+      (mod (and multiplicative ws "/" ws primary) #'/)
+      (mod (and multiplicative ws "%" ws primary) #'mod)
+      primary))
+
+(defrule primary (or integer (and "(" ws additive ws ")")))
+
+(defrule integer (mod (cap (and (? "-") (+ (cc "0-9")))) #'parse-integer))
+
+(defrule ws (* (cc #.(format nil " ~a~a~a" #\cr #\lf #\tab))))
+
+(defparser parse-arithmetic arithmetic)
+
+(format t "(1 + 2) * 3 * 4 - (5 / 6) = ~a~%" (parse-arithmetic "(1 + 2) * 3 * 4 - (5 / 6)"))
